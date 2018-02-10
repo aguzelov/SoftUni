2e1c5644f9938c60247fcdbc6d3c1ee6d27e5668
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-10 12:37:50 +0200

    Add 04. Treasure Map

---
+++ Advanced C#/Exam Preparation/Exam Retake - 3 September 2017/04. Treasure Map/TreasureMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class TreasureMap
+{
+    public static void Main()
+    {
+        string pattern = @"((?<sign>#)|!)[^#|!]*?(?<![a-zA-Z0-9])(?<name>[a-zA-Z]{4})(?![a-zA-Z0-9])[^#|!]*(?<!\d)(?<number>\d{3})-(?<password>\d{4}|\d{6})(?!\d)[^#|!]*?(?(sign)!|#)";
+
+        int numberOfInput = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfInput; i++)
+        {
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Match correctMatch = matches[matches.Count / 2];
+
+            string streetName = correctMatch.Groups["name"].Value;
+            string streetNumber = correctMatch.Groups["number"].Value;
+            string password = correctMatch.Groups["password"].Value;
+
+            Console.WriteLine($"Go to str. {streetName} {streetNumber}. Secret pass: {password}.");
+        }
+    }
+}
