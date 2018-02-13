6f533a408626f8fea55cc9af1ad0ed5bbf92b32f
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-13 18:28:14 +0200

    Add Problem 7. Speed Racing

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/SpeedRacing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Car
+{
+
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumption { get; set; }
+    public double DistanceTraveled { get; set; }
+
+    public Car(string model, double fuelAmount, double fuelConsumption)
+    {
+        Model = model;
+        FuelAmount = fuelAmount;
+        FuelConsumption = fuelConsumption;
+        DistanceTraveled = 0;
+    }
+
+    public void Move(double amountOfKm)
+    {
+        double neededFuel = amountOfKm * FuelConsumption;
+        if (neededFuel <= FuelAmount)
+        {
+            FuelAmount -= neededFuel;
+            DistanceTraveled += amountOfKm;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Model} {FuelAmount:F2} {DistanceTraveled}";
+    }
+}
---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/SpeedRacing/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main()
+    {
+        Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string model = input[0];
+            double fuelAmount = double.Parse(input[1]);
+            double fuelConsumption = double.Parse(input[2]);
+
+            if (!cars.ContainsKey(model))
+            {
+                cars.Add(model, new Car(model, fuelAmount, fuelConsumption));
+            }
+        }
+
+        string command = String.Empty;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] moveParam = command.Split();
+
+            string model = moveParam[1];
+            double amountOfKm = double.Parse(moveParam[2]);
+
+            cars[model].Move(amountOfKm);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.Value.ToString());
+        }
+    }
+}
