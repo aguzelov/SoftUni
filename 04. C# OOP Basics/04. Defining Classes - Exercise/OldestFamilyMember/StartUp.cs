7e7a0a64540e62b4fe145cab17961d9da45299a9
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-13 18:01:13 +0200

    Add Problem 3. Oldest Family Member

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace OldestFamilyMember
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MethodInfo oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
+            MethodInfo addMemberMethod = typeof(Family).GetMethod("AddMember");
+            if (oldestMemberMethod == null || addMemberMethod == null)
+            {
+                throw new Exception();
+            }
+
+            Family family = new Family();
+
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
