4d4f175d7034571235cf2354af1513e5d4b4713c
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-13 18:01:46 +0200

    Problem 5. Date Modifier

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/DateModifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class DateModifier
+{
+    private DateTime firstDate;
+    private DateTime secondDate;
+
+    public DateModifier()
+    {
+    }
+
+    public DateModifier(string firstDate , string secondDate)
+    {
+        this.firstDate = DateTime.ParseExact(firstDate, "yyyy MM dd",
+            System.Globalization.CultureInfo.InvariantCulture);
+
+        this.secondDate = DateTime.ParseExact(secondDate, "yyyy MM dd",
+            System.Globalization.CultureInfo.InvariantCulture);
+    }
+
+    public int DateDifference(string firstDate, string secondDate)
+    {
+        this.firstDate = DateTime.ParseExact(firstDate, "yyyy MM dd",
+            System.Globalization.CultureInfo.InvariantCulture);
+
+        this.secondDate = DateTime.ParseExact(secondDate, "yyyy MM dd",
+            System.Globalization.CultureInfo.InvariantCulture);
+
+        return Math.Abs((this.secondDate - this.firstDate).Days);
+    }
+}
---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/DateModifier/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string firstDate = Console.ReadLine();
+        string secondDate = Console.ReadLine();
+
+        DateModifier dateModifier = new DateModifier();
+        Console.WriteLine(dateModifier.DateDifference(firstDate, secondDate));
+    }
+}
