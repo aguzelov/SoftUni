148d6653def0296067ffcaa8eee8b7b774c37b63
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-13 21:54:52 +0200

    Add Problem 9. Rectangle Intersection

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿public class Rectangle
+{
+    public string ID { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public Point TopLeft { get; set; }
+
+    public Rectangle(string iD, double width, double height, Point topLeft)
+    {
+        ID = iD;
+        Width = width;
+        Height = height;
+        TopLeft = topLeft;
+
+    }
+
+    public bool IsIntersectWith(Rectangle other)
+    {
+        return other.TopLeft.X + other.Width >= this.TopLeft.X &&
+               other.TopLeft.X <= this.TopLeft.X + this.Width &&
+               other.TopLeft.Y >= this.TopLeft.Y - this.Height &&
+               other.TopLeft.Y - other.Height <= this.TopLeft.Y;
+    }
+}
---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/RectangleIntersection/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    static void Main()
+    {
+        int[] rectangleParam = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int numberOfRectangle = rectangleParam[0];
+        int numberOfIntersection = rectangleParam[1];
+
+        Dictionary<string, Rectangle> rectangles = new Dictionary<string, Rectangle>();
+
+        for (int i = 0; i < numberOfRectangle; i++)
+        {
+            string[] inputArray = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string id = inputArray[0];
+            double width = double.Parse(inputArray[1]);
+            double height = double.Parse(inputArray[2]);
+            double topLeftX = double.Parse(inputArray[3]);
+            double topLeftY = double.Parse(inputArray[4]);
+            
+            rectangles.Add(id ,new Rectangle(id, width, height, new Point(topLeftX, topLeftY)));
+        }
+
+        for (int i = 0; i < numberOfIntersection; i++)
+        {
+            string[] pair = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstId = pair[0];
+            string secondId = pair[1];
+            if (rectangles.ContainsKey(firstId))
+            {
+                Rectangle first = rectangles[firstId];
+                if (rectangles.ContainsKey(secondId))
+                {
+                    Rectangle second = rectangles[secondId];
+                    Console.WriteLine(first.IsIntersectWith(second).ToString().ToLower());
+                }
+            }
+        }
+    }
+}
