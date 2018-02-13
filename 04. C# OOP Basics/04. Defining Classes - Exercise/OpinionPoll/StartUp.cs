ba5d84eda0d16194fa3dbaa1435865ce23860825
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-13 18:01:33 +0200

    Add Problem 4. Opinion Poll

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/OpinionPoll/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> persons = new List<Person>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] personalInformation = Console.ReadLine().Split();
+
+            string name = personalInformation[0];
+            int age = int.Parse(personalInformation[1]);
+
+            persons.Add(new Person(name, age));
+        }
+
+        foreach (var person in persons.Where(a=> a.age > 30).OrderBy(a => a.name))
+        {
+            Console.WriteLine($"{person.name} - {person.age}");
+        }
+    }
+}
