28c47e952cff182df868c8d4c2f41c534ab5dcd7
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-15 20:17:24 +0200

    Add Problem 15. Drawing tool

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/DrawingTool/CorDraw.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class CorDraw
+{
+    private double width;
+    private double height;
+
+    public CorDraw(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void Draw()
+    {
+        for (int i = 0; i < (int)height; i++)
+        {
+            Console.Write("|");
+
+            if ((i > 0 && i < height - 1))
+            {
+                Console.Write(new string(' ', (int)width));
+            }
+            else
+            {
+                Console.Write(new string('-', (int)width));
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/DrawingTool/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main()
+    {
+        string shape = Console.ReadLine();
+        double width = double.Parse(Console.ReadLine());
+
+        double height = 0;
+
+        switch (shape)
+        {
+            case "Square":
+                height = width;
+                break;
+            case "Rectangle":
+                height = double.Parse(Console.ReadLine());
+                break;
+        }
+
+        CorDraw corDraw = new CorDraw(width, height);
+        corDraw.Draw();
+    }
+}
