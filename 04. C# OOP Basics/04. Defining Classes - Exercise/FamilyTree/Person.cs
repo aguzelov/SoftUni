70ffc8cd5f06d300a95098d5efc2ddf6badbdc8b
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-15 18:53:31 +0200

    Add Problem 13. Family Tree

---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/FamilyTree/Person.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Birthday { get; set; }
+    public List<Person> Childrens { get; set; }
+
+    public Person()
+    {
+        Childrens = new List<Person>();
+    }
+
+    public Person(string nameOrBirthday) : this()
+    {
+        if (nameOrBirthday.Contains("/"))
+        {
+            Birthday = nameOrBirthday;
+        }
+        else
+        {
+            Name = nameOrBirthday;
+        }
+    }
+
+    public Person(string name, string birthday) : this()
+    {
+        Name = name;
+        Birthday = birthday;
+    }
+
+    public void AddName(string name)
+    {
+        Name = name;
+    }
+
+    public void AddBirthday(string birthday)
+    {
+        Birthday = birthday;
+    }
+
+    public void AddChild(Person child)
+    {
+        Childrens.Add(child);
+    }
+
+    public void AddChildren(List<Person> children)
+    {
+        Childrens.AddRange(children);
+    }
+
+    public void UpdateChiren(Person person)
+    {
+        Person child = Childrens.FirstOrDefault(n => n.Name == person.Name);
+
+        if (child == null)
+        {
+            child = Childrens.FirstOrDefault(n => n.Birthday == person.Birthday);
+            if(child != null)
+            {
+                child.Name = person.Name;
+            }
+
+            return;
+        }
+
+        child.Birthday = person.Birthday;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {Birthday}";
+    }
+
+    public bool Equals(Person other)
+    {
+        return this.Name == other.Name && this.Birthday == other.Birthday;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var person = obj as Person;
+        if (person == null) return false;
+
+        return this.Equals(person);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
---
+++ 04. C# OOP Basics/04. Defining Classes - Exercise/FamilyTree/StartUp.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class StartUp
+{
+    static void Main()
+    {
+        string personInput = Console.ReadLine();
+        
+        List<Person> persons = new List<Person>();
+
+        string input = string.Empty;
+        while ((input = Console.ReadLine().Trim()) != "End")
+        {   
+            if (input.Contains(" - "))
+            {
+                string[] pair = input.Split(" - ");
+
+                //Parent
+                Person parent = null;
+                if (pair[0].Contains("/"))
+                {
+                    string parentBirthday = pair[0];
+                    parent = persons.FirstOrDefault(p => p.Birthday == parentBirthday);
+                    
+                }
+                else
+                {
+                    string parentName = pair[0];
+                    parent = persons.FirstOrDefault(p => p.Name == parentName);
+                    
+                }
+
+                if(parent == null)
+                {
+                    parent = new Person(pair[0]);
+                    persons.Add(parent);
+                }
+
+                //Child
+                Person child = new Person(pair[1]);
+                
+                parent.AddChild(child);
+            }
+            else
+            {
+                string[] personInfo = input.Split(" ");
+                string name = personInfo[0] + " " + personInfo[1];
+                string birthday = personInfo[2];
+
+                bool isFound = false;
+                for(int i = 0; i < persons.Count; i++)
+                {
+                    if(persons[i].Name == name)
+                    {
+                        persons[i].Birthday = birthday;
+                        isFound = true;
+                    }
+
+                    if(persons[i].Birthday == birthday)
+                    {
+                        persons[i].Name = name;
+                        isFound = true;
+                    }
+
+                    persons[i].UpdateChiren(new Person(name, birthday));
+                }
+
+                if (!isFound)
+                {
+                    persons.Add(new Person(name, birthday));
+                }
+                
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Person personToSearch = null;
+        if (personInput.Contains("/"))
+        {
+            personToSearch = persons.FirstOrDefault(p => p.Birthday == personInput);
+        }
+        else
+        {
+            personToSearch = persons.FirstOrDefault(p => p.Name == personInput);
+        }
+
+        sb.AppendLine(personToSearch.ToString());
+
+        sb.AppendLine("Parents:");
+        
+        foreach(var parent in persons.Where(p=> p.Childrens.Contains(personToSearch)))
+        {
+            sb.AppendLine(parent.ToString());
+        }
+
+        sb.AppendLine("Children:");
+
+        foreach (var child in persons.FirstOrDefault(p => p.Name == personToSearch.Name && p.Birthday == personToSearch.Birthday).Childrens)
+        {
+            sb.AppendLine(child.ToString());
+        }
+
+        Console.Write(sb.ToString());
+    }
+}
