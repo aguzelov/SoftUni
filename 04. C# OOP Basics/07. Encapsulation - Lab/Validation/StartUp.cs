f2b2d138aba89aaccaea32bea1b3c5b2772ecc7c
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-19 22:20:56 +0200

    Add 03. Validation

---
+++ 04. C# OOP Basics/07. Encapsulation - Lab/Validation/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+
+        var lines = int.Parse(Console.ReadLine());
+        var persons = new List<Person>();
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            Person person = null;
+            try
+            {
+                person = new Person(cmdArgs[0],
+                                   cmdArgs[1],
+                                   int.Parse(cmdArgs[2]),
+                                   decimal.Parse(cmdArgs[3]));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+           
+            persons.Add(person);
+        }
+        var bonus = decimal.Parse(Console.ReadLine());
+        persons.ForEach(p => p.IncreaseSalary(bonus));
+        persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+    }
+}
