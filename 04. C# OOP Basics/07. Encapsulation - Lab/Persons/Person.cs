89ea67fb2d23b222b3e81221420db294d31ec923
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-19 21:55:57 +0200

    Add 01. Persons

---
+++ 04. C# OOP Basics/07. Encapsulation - Lab/Persons/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public int Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        set { this.lastName = value; }
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set { this.firstName = value; }
+    }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+        this.lastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        this.age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} is {this.age} years old.";
+    }
+}
