1e1833f4fe85d4ddada382de797ba606ae0d1081
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-19 20:52:32 +0200

    Add 07. Family Tree

---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/FamilyTree/FamilyTreeBuilder.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class FamilyTreeBuilder
+{
+    private List<Person> FamilyTree { get; set; }
+    private Person MainPerson { get; set; }
+
+    public FamilyTreeBuilder(string mainPersonInput)
+    {
+        FamilyTree = new List<Person>();
+        MainPerson = Person.CreatePerson(mainPersonInput);
+        FamilyTree.Add(MainPerson);
+    }
+
+    public string Build()
+    {
+        StringBuilder resultBuilder = new StringBuilder();
+        resultBuilder.AppendLine(this.MainPerson.ToString());
+
+        resultBuilder.AppendLine("Parents:");
+        foreach (var parent in this.MainPerson.Parents)
+        {
+            resultBuilder.AppendLine(parent.ToString());
+        }
+
+        resultBuilder.AppendLine("Children:");
+        foreach (var child in this.MainPerson.Children)
+        {
+            resultBuilder.AppendLine(child.ToString());
+        }
+
+        string result = resultBuilder.ToString().TrimEnd();
+        return result;
+    }
+
+    private Person FindOrCreate(string personInput)
+    {
+        var person = this.FamilyTree
+            .FirstOrDefault(c => c.Name == personInput || c.Birthday == personInput);
+
+        if (person == null)
+        {
+            person = Person.CreatePerson(personInput);
+            this.FamilyTree.Add(person);
+        }
+
+        return person;
+    }
+
+    public void SetParentChildRelation(string parentInput, string childInput)
+    {
+        Person parent = FindOrCreate(parentInput);
+
+        SetChild(parent, childInput);
+    }
+
+    private void SetChild(Person parent, string childInput)
+    {
+        var child = FindOrCreate(childInput);
+
+        parent.Children.Add(child);
+        child.Parents.Add(parent);
+    }
+
+    public void SetFullInfo(string name, string birthday)
+    {
+        var person = this.FamilyTree
+            .FirstOrDefault(c => c.Name == name || c.Birthday == birthday);
+
+        if (person == null)
+        {
+            person = new Person();
+            this.FamilyTree.Add(person);
+        }
+
+        person.Name = name;
+        person.Birthday = birthday;
+
+        this.CheckForDuplicate(person);
+    }
+
+    private void CheckForDuplicate(Person person)
+    {
+        string name = person.Name;
+        string birthday = person.Birthday;
+
+        Person duplicate = this.FamilyTree
+            .Where(p => p.Name == name || p.Birthday == birthday)
+            .Skip(1)
+            .FirstOrDefault();
+
+        if (duplicate != null)
+        {
+            this.RemoveDuplicate(person, duplicate);
+        }
+    }
+
+    private void RemoveDuplicate(Person person, Person duplicate)
+    {
+        this.FamilyTree.Remove(duplicate);
+
+        person.Parents.AddRange(duplicate.Parents);
+        //person.Parents = person.Parents.Distinct().ToList();
+        foreach (var parent in duplicate.Parents)
+        {
+            ReplaceDuplicate(person, duplicate, parent.Children);
+        }
+
+        person.Children.AddRange(duplicate.Children);
+        //person.Children = person.Children.Distinct().ToList();
+        foreach (var child in duplicate.Children)
+        {
+            ReplaceDuplicate(person, duplicate, child.Parents);
+        }
+    }
+
+    private static void ReplaceDuplicate(Person original, Person duplicate, List<Person> collection)
+    {
+        int duplicateIndex = collection.IndexOf(duplicate);
+        if (duplicateIndex > -1)
+        {
+            collection[duplicateIndex] = original;
+        }
+        else
+        {
+            collection.Add(original);
+        }
+    }
+}
---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/FamilyTree/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        string mainPersonInput = Console.ReadLine();
+        FamilyTreeBuilder familyTreeBuilder = new FamilyTreeBuilder(mainPersonInput);
+
+        
+        while (ParseInput(familyTreeBuilder))
+        {
+        }
+
+        string familyTree = familyTreeBuilder.Build();
+        Console.WriteLine(familyTree);
+    }
+
+    private static bool ParseInput(FamilyTreeBuilder familyTreeBuilder)
+    {
+        string input = Console.ReadLine();
+        if (input == "End") return false;
+
+        string[] tokens = input.Split(" - ");
+        if (tokens.Length > 1)
+        {
+            ParentChild(familyTreeBuilder, tokens);
+        }
+        else
+        {
+            FullInfo(familyTreeBuilder, tokens);
+        }
+
+        return true;
+    }
+
+    private static void FullInfo(FamilyTreeBuilder familyTreeBuilder, string[] tokens)
+    {
+        tokens = tokens[0].Split();
+        string name = $"{tokens[0]} {tokens[1]}";
+        string birthday = tokens[2];
+
+        familyTreeBuilder.SetFullInfo(name, birthday);
+    }
+
+    private static void ParentChild(FamilyTreeBuilder familyTreeBuilder, string[] tokens)
+    {
+        string parentInput = tokens[0];
+        string childInput = tokens[1];
+        familyTreeBuilder.SetParentChildRelation(parentInput, childInput);
+    }
+}
