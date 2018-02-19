d0947c371b4c36fc4fd698ecc317aa24bdad2898
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-19 17:06:08 +0200

    Add 03. Jedi Galaxy

---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/JediGalaxy/Coordinate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+class Coordinate
+{
+    private int row;
+
+    public int Row
+    {
+        get { return row; }
+        set { row = value; }
+    }
+
+    private int col;
+
+    public int Col
+    {
+        get { return col; }
+        set { col = value; }
+    }
+
+    public Coordinate(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Coordinate(string command)
+    {
+        int[] coordinates = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        this.row = coordinates[0];
+        this.col = coordinates[1];
+    }
+
+    public Coordinate(Func<string> readCoordinates)
+    {
+        int[] coordinates = readCoordinates().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        this.row = coordinates[0];
+        this.col = coordinates[1];
+    }
+}
---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/JediGalaxy/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        int[] dimestions = GetInputAsIntArray(Console.ReadLine);
+
+        int row = dimestions[0];
+        int col = dimestions[1];
+
+        int[,] matrix = new int[row, col];
+
+        int value = 0;
+        for (int currentRow = 0; currentRow < row; currentRow++)
+        {
+            for (int currentCol = 0; currentCol < col; currentCol++)
+            {
+                matrix[currentRow, currentCol] = value++;
+            }
+        }
+
+        long sum = 0;
+        while (GetCoordinates(out Coordinate ivoCoordinate, out Coordinate evilCoordinate))
+        {
+
+            while (evilCoordinate.Row >= 0 && evilCoordinate.Col >= 0)
+            {
+                if (IsInside(matrix, evilCoordinate))
+                {
+                    matrix[evilCoordinate.Row, evilCoordinate.Col] = 0;
+                }
+                evilCoordinate.Row--;
+                evilCoordinate.Col--;
+            }
+
+            while (ivoCoordinate.Row >= 0 && ivoCoordinate.Col < matrix.GetLength(1))
+            {
+                if (IsInside(matrix, ivoCoordinate))
+                {
+                    sum += matrix[ivoCoordinate.Row, ivoCoordinate.Col];
+                }
+
+                ivoCoordinate.Col++;
+                ivoCoordinate.Row--;
+            }
+        }
+
+        Console.WriteLine(sum);
+
+    }
+
+    private static bool GetCoordinates(out Coordinate ivoCoordinate, out Coordinate evilCoordinate)
+    {
+        ivoCoordinate = null;
+        evilCoordinate = null;
+
+        string coordinate = Console.ReadLine();
+        if (coordinate == "Let the Force be with you") return false;
+
+        ivoCoordinate = new Coordinate(coordinate);
+        evilCoordinate = new Coordinate(Console.ReadLine);
+
+        return true;
+    }
+
+    private static bool IsInside(int[,] matrix, Coordinate coordinate)
+    {
+        return coordinate.Row >= 0 &&
+               coordinate.Row < matrix.GetLength(0) &&
+               coordinate.Col >= 0 &&
+               coordinate.Col < matrix.GetLength(1);
+    }
+
+    private static int[] GetInputAsIntArray(Func<string> readCoordinates)
+    {
+        return readCoordinates().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    }
+}
