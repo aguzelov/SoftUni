31b14f346057c67bf3d4cf95d13e2118d7906da5
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-19 16:27:19 +0200

    Add 02. Car Salesman

---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/CarSalesman/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Car
+{
+    public string Model { get; set; }
+    public Engine Engine { get; set; }
+    public string Weight { get; set; }
+    public string Color { get; set; }
+
+    public Car(string model, Engine engine)
+    {
+        Model = model;
+        Engine = engine;
+        Weight = "n/a";
+        Color = "n/a";
+    }
+
+    public Car(string model, Engine engine, string optional) : this(model, engine)
+    {
+        if (double.TryParse(optional, out double weight))
+        {
+            Weight = weight.ToString();
+        }
+        else
+        {
+            Color = optional;
+        }
+    }
+
+    public Car(string model, Engine engine, string weight, string color) : this(model, engine)
+    {
+        Weight = weight;
+        Color = color;
+    }
+
+    public override string ToString()
+    {
+        return $"{Model}:{Environment.NewLine}" +
+               $"  {Engine.Model}:{Environment.NewLine}" +
+               $"    Power: {Engine.Power}{Environment.NewLine}" +
+               $"{Engine.ToString()}" +
+               $"    Weight: {Weight}{Environment.NewLine}" +
+               $"    Color: {Color}";
+    }
+}
---
+++ 04. C# OOP Basics/06. Working with Abstraction - Exercise/CarSalesman/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Engine
+{
+    public string Model { get; set; }
+    public double Power { get; set; }
+    public string Displacement { get; set; }
+    public string Efficiency { get; set; }
+    
+    public Engine(string model, double power)
+    {
+        Model = model;
+        Power = power;
+        Displacement = "n/a";
+        Efficiency = "n/a";
+    }
+
+    public Engine(string model, double power, string optional) : this(model, power)
+    {
+        if (double.TryParse(optional, out double displacement))
+        {
+            Displacement = displacement.ToString();
+        }
+        else
+        {
+            Efficiency = optional;
+        }
+    }
+    
+    public Engine(string model, double power, string displacement, string efficiency) : this(model, power)
+    {
+        Displacement = displacement;
+        Efficiency = efficiency;
+    }
+
+    public override string ToString()
+    {
+        return $"    Displacement: {Displacement}{Environment.NewLine}" +
+               $"    Efficiency: {Efficiency}{Environment.NewLine}";
+    }
+}
