ba1502bdc2b807394ef7f24594972ee820d89cf8
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-20 21:55:58 +0200

    Add 02. Book Shop

---
+++ 04. C# OOP Basics/10. Inheritance - Exercise/BookShop/Book.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+
+public class Book
+{
+    private string title;
+    private string author;
+    private decimal price;
+
+    public string Author
+    {
+        get
+        {
+            return this.author;
+        }
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentException("Author not valid!");
+            }
+
+            string[] authorNames = value.Split();
+
+            if (char.IsDigit(authorNames[authorNames.Length - 1][0]))
+            {
+                throw new ArgumentException("Author not valid!");
+            }
+
+            this.author = value;
+        }
+    }
+
+    public string Title
+    {
+        get
+        {
+            return this.title;
+        }
+        set
+        {
+            if (value == null || value.Length < 3)
+            {
+                throw new ArgumentException("Title not valid!");
+            }
+
+            this.title = value;
+        }
+    }
+
+    public virtual decimal Price
+    {
+        get
+        {
+            return this.price;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Price not valid!");
+            }
+
+            this.price = value;
+        }
+    }
+
+    public Book(string author, string title, decimal price)
+    {
+        Title = title;
+        Author = author;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Type: ").AppendLine(this.GetType().Name)
+            .Append("Title: ").AppendLine(this.Title)
+            .Append("Author: ").AppendLine(this.Author)
+            .Append("Price: ").Append($"{this.Price:f1}")
+            .AppendLine();
+
+        return sb.ToString();
+    }
+}
---
+++ 04. C# OOP Basics/10. Inheritance - Exercise/BookShop/GoldenEditionBook.cs
@@ -0,0 +1,14 @@
+﻿public class GoldenEditionBook : Book
+{
+    public override decimal Price
+    {
+        get
+        {
+            return base.Price * (decimal)1.3;
+        }
+    }
+
+    public GoldenEditionBook(string author, string title, decimal price) : base(author, title, price)
+    {
+    }
+}
