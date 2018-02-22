83331b56dd989d2bf7f3a4b45ec9ccfb0d0a71a8
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-22 23:49:34 +0200

    Add 05. Stack of Strings

---
+++ 04. C# OOP Basics/09. Inheritance - Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public StackOfStrings()
+    {
+        this.data = new List<string>();
+    }
+
+    public void Push(string item)
+    {
+        if (!string.IsNullOrWhiteSpace(item))
+        {
+            this.data.Add(item);
+        }
+    }
+
+    public string Pop()
+    {
+        if (!this.IsEmpty())
+        {
+            int index = this.data.Count - 1;
+            string element = this.data[index];
+            this.data.RemoveAt(index);
+            return element;
+        }
+        return null;
+    }
+
+    public string Peek()
+    {
+        if (!this.IsEmpty())
+        {
+            int index = this.data.Count - 1;
+            string element = this.data[index];
+            return element;
+        }
+        return null;
+    }
+
+    public bool IsEmpty()
+    {
+        return this.data.Count == 0;
+    }
+}
