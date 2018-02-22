18dc2b73066e14ba1187b77efe0522c30df5c48c
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-22 23:36:31 +0200

    Add 04. Random List

---
+++ 04. C# OOP Basics/09. Inheritance - Lab/RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RandomList : List<string>
+{
+    public RandomList() : base()
+    {
+    }
+
+    public string RandomString()
+    {
+        Random r = new Random();
+        int index = r.Next(0, base.Count); //for ints
+
+        string element = base[index];
+        base.RemoveAt(index);
+        return element;
+    }
+}
