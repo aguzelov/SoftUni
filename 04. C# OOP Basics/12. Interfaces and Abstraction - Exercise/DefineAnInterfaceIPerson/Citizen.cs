47d29a947d78b0407a9d01cc862d6c415d58a0f8
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-24 15:04:26 +0200

    Add 01. Define an Interface IPerson and 02. Multiple Implementation

---
+++ 04. C# OOP Basics/12. Interfaces and Abstraction - Exercise/DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,67 @@
+﻿internal class Citizen : IPerson, IIdentifiable, IBirthable
+{
+    private string name;
+    private int age;
+    private string id;
+    private string birthdate;
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    public string Id
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            this.id = value;
+        }
+    }
+
+    public string Birthdate
+    {
+        get
+        {
+            return this.birthdate;
+        }
+        set
+        {
+            this.birthdate = value;
+        }
+    }
+
+    public Citizen(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public Citizen(string name, int age, string id, string birthdate) : this(name, age)
+    {
+        Id = id;
+        Birthdate = birthdate;
+    }
+}
