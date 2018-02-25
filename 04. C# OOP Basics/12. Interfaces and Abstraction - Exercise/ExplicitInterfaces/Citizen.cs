087997943336f1bbfdb84501fa7fd9a74afec11f
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-02-25 22:43:58 +0200

    Add 10. Explicit Interfaces

---
+++ 04. C# OOP Basics/12. Interfaces and Abstraction - Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,25 @@
+﻿public class Citizen : IResident, IPerson
+{
+    public string Name { get; set; }
+
+    public string Country { get; set; }
+
+    public int Age { get; set; }
+
+    public Citizen(string name, string country, int age)
+    {
+        Name = name;
+        Country = country;
+        Age = age;
+    }
+
+    string IResident.GetName()
+    {
+        return $"Mr/Ms/Mrs {Name}";
+    }
+
+    string IPerson.GetName()
+    {
+        return Name;
+    }
+}
---
+++ 04. C# OOP Basics/12. Interfaces and Abstraction - Exercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string input = string.Empty;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] tokens = input.Split();
+            string name = tokens[0];
+            string country = tokens[1];
+            int age = int.Parse(tokens[2]);
+
+            Citizen citizen = new Citizen(name, country, age);
+
+            IPerson ip = (IPerson)citizen;
+            IResident ir = (IResident)citizen;
+
+            Console.WriteLine(ip.GetName());
+            Console.WriteLine(ir.GetName());
+        }
+    }
+}
