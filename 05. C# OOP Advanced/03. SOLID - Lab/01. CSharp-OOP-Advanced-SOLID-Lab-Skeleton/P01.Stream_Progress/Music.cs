cec4071dd3bd59675192e41953d5692331846411
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-21 22:48:52 +0200

    Add Stream Progress

---
+++ 05. C# OOP Advanced/03. SOLID - Lab/01. CSharp-OOP-Advanced-SOLID-Lab-Skeleton/P01.Stream_Progress/Music.cs
@@ -0,0 +1,22 @@
+﻿using P01.Stream_Progress.Interfaces;
+
+namespace P01.Stream_Progress
+{
+    public class Music : IStreamable
+    {
+        private string artist;
+        private string album;
+
+        public Music(string artist, string album, int length, int bytesSent)
+        {
+            this.artist = artist;
+            this.album = album;
+            this.Length = length;
+            this.BytesSent = bytesSent;
+        }
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
---
+++ 05. C# OOP Advanced/03. SOLID - Lab/01. CSharp-OOP-Advanced-SOLID-Lab-Skeleton/P01.Stream_Progress/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            //File file = new File("fileName",10,10);
+            Music music = new Music("artist", "album", 10, 10);
+            StreamProgressInfo streamProgressInfo = new StreamProgressInfo(music);
+
+            Console.WriteLine(streamProgressInfo.CalculateCurrentPercent());
+        }
+    }
+}
