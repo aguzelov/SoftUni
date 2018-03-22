5fc20c14e4bc5c9c5683f9f1730936f9b34db5c0
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 00:04:22 +0200

    Add Truple

---
+++ 05. C# OOP Advanced/06. Generics - Exercise/Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        string[] inputArgs = Console.ReadLine().Split();
+
+        string item1 = inputArgs[0] + " " + inputArgs[1];
+        string item2 = inputArgs[2];
+
+        Truple<string, string> firstTruple = new Truple<string, string>(item1,item2);
+
+        inputArgs = Console.ReadLine().Split();
+        string personName = inputArgs[0];
+        int beerAmount = int.Parse(inputArgs[1]);
+
+        Truple<string,int> secondTruple = new Truple<string, int>(personName, beerAmount);
+
+        inputArgs = Console.ReadLine().Split();
+        int firstNum = int.Parse(inputArgs[0]);
+        double secondNum = double.Parse(inputArgs[1]);
+
+        Truple<int,double> thirdTruple = new Truple<int, double>(firstNum, secondNum);
+
+        Console.WriteLine(firstTruple);
+        Console.WriteLine(secondTruple);
+        Console.WriteLine(thirdTruple);
+    }
+}
---
+++ 05. C# OOP Advanced/06. Generics - Exercise/Tuple/Truple.cs
@@ -0,0 +1,19 @@
+﻿public class Truple<T,V>
+{
+    private T item1;
+    private V item2;
+
+    public Truple(T item1, V item2)
+    {
+        this.Item1 = item1;
+        this.Item2 = item2;
+    }
+
+    public T Item1 { get; set; }    
+    public V Item2 { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Item1} -> {this.Item2}";
+    }
+}
