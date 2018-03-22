f52a4ae48d2c08a99bcc20cc833bb0f99c344fff
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-22 14:46:17 +0200

    Add 2. Generic Array Creator

---
+++ 05. C# OOP Advanced/05. Generics - Lab/ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,7 @@
+﻿public class ArrayCreator
+{
+    public static T[] Create<T>(int length, T item)
+    {
+        return new T[length];
+    }
+}
---
+++ 05. C# OOP Advanced/05. Generics - Lab/ArrayCreator/StartUp.cs
@@ -0,0 +1,8 @@
+﻿public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+    }
+}
