46fc655a9aaf6e8d35d9d2664cfd39be6060f450
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 00:19:51 +0200

    Add Threeuple

--- 05. C# OOP Advanced/06. Generics - Exercise/Tuple/StartUp.cs
+++ 05. C# OOP Advanced/06. Generics - Exercise/Tuple/StartUp.cs
@@ -8,20 +8,26 @@ public class StartUp
 
         string item1 = inputArgs[0] + " " + inputArgs[1];
         string item2 = inputArgs[2];
+        string item3 = inputArgs[3];
 
-        Truple<string, string> firstTruple = new Truple<string, string>(item1,item2);
+        Threeuple<string, string, string> firstTruple = 
+            new Threeuple<string, string, string>(item1, item2, item3);
 
         inputArgs = Console.ReadLine().Split();
         string personName = inputArgs[0];
         int beerAmount = int.Parse(inputArgs[1]);
+        bool drunkText = inputArgs[2] == "drunk" ? true : false;
 
-        Truple<string,int> secondTruple = new Truple<string, int>(personName, beerAmount);
+        Threeuple<string, int, bool> secondTruple = 
+            new Threeuple<string, int, bool>(personName, beerAmount, drunkText);
 
         inputArgs = Console.ReadLine().Split();
-        int firstNum = int.Parse(inputArgs[0]);
-        double secondNum = double.Parse(inputArgs[1]);
+        string name = inputArgs[0];
+        double balance = double.Parse(inputArgs[1]);
+        string bankName = inputArgs[2];
 
-        Truple<int,double> thirdTruple = new Truple<int, double>(firstNum, secondNum);
+        Threeuple<string, double, string> thirdTruple = 
+            new Threeuple<string, double, string>(name, balance, bankName);
 
         Console.WriteLine(firstTruple);
         Console.WriteLine(secondTruple);
---
+++ 05. C# OOP Advanced/06. Generics - Exercise/Tuple/Threeuple.cs
@@ -0,0 +1,18 @@
+﻿public class Threeuple<T,V,M> : Truple<T,V>
+{
+    private M item3;
+
+    public Threeuple(T item1, V item2, M item3)
+    :base(item1,item2)
+    {
+        this.Item3 = item3;
+    }
+
+    public M Item3 { get; set; }
+
+    public override string ToString()
+    {
+        
+        return $"{base.ToString()} -> {this.Item3}";
+    }
+}
