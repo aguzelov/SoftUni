138b9a39fb45dae5c9b7e295b4ef71a7c4cc840e
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 00:04:10 +0200

    Add Box

---
+++ 05. C# OOP Advanced/06. Generics - Exercise/Generic Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Box<T> : IComparable where T : IComparable<T>
+{
+    private T value;
+
+    public Box(T value)
+    {
+        this.value = value;
+    }
+
+    public T Value
+    {
+        get { return this.value; }
+        private set { this.value = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.value.GetType().FullName}: {this.value}";
+    }
+
+    public int CompareTo(object obj)
+    {
+        var item = obj is T ? (T) obj : default(T);
+
+        var result = this.Value.CompareTo(item);
+
+        return result;
+    }
+}
---
+++ 05. C# OOP Advanced/06. Generics - Exercise/Generic Box/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int numberOfBoxInputs = int.Parse(Console.ReadLine());
+
+        List<Box<double>> boxs = new List<Box<double>>();
+        for (int i = 0; i < numberOfBoxInputs; i++)
+        {
+            double input = double.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>(input);
+            boxs.Add(box);
+        }
+
+        double element = double.Parse(Console.ReadLine());
+        Console.WriteLine(Compare(boxs, element));
+    }
+
+    public static int Compare<T>(List<Box<T>> boxs, T elemen)
+    where T : IComparable<T>
+    {
+        int counter = 0;
+
+        foreach (var box in boxs)
+        {
+            if (box.CompareTo(elemen) > 0) counter++;
+        }
+
+        return counter;
+    }
+}
