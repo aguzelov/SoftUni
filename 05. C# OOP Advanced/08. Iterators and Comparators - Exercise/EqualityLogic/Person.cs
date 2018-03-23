d6131535ad9427f63e2fd5c1820896a9b99833c1
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 20:36:18 +0200

    Add 07. Equality Logic

---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Person : IComparable
+{
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int Compare(Person x, Person y)
+    {
+        int result = x.Name.CompareTo(y.Name);
+
+        if (result == 0)
+        {
+            result = x.Age.CompareTo(y.Age);
+        }
+
+        return result;
+    }
+
+    public int CompareTo(object obj)
+    {
+        if (!(obj is Person person))
+        {
+            throw new NullReferenceException();
+        }
+
+        return Compare(this, person);
+    }
+
+    private bool Equals(Person other)
+    {
+        return this.Name.Equals(other.Name) && this.Age.Equals(other.Age);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Person person))
+        {
+            throw new NullReferenceException();
+        }
+
+        return this.Equals(person);
+    }
+
+    public override int GetHashCode()
+    {
+        int hashCode = this.GetHashCodeOnProperties(this);
+        return hashCode;
+    }
+
+    public int GetHashCodeOnProperties(Person inspect)
+    {
+        return GetListHashCode(inspect.GetType().GetProperties().Select(o => o.GetValue(inspect)));
+    }
+
+    public int GetListHashCode<T>(IEnumerable<T> sequence)
+    {
+        return sequence
+            .Select(item => item.GetHashCode())
+            .Aggregate((total, nextCode) => total ^ nextCode);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} {this.Age}";
+    }
+}
---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/EqualityLogic/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int numberOfPerson = int.Parse(Console.ReadLine());
+
+        SortedSet<Person> sortedSet = new SortedSet<Person>();
+        HashSet<Person> hashSet = new HashSet<Person>();
+
+        while (--numberOfPerson >= 0)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            string name = tokens[0];
+            int age = int.Parse(tokens[1]);
+
+            Person person = new Person(name, age);
+
+            sortedSet.Add(person);
+            hashSet.Add(person);
+        }
+
+        Console.WriteLine(sortedSet.Count);
+        Console.WriteLine(hashSet.Count);
+    }
+}
