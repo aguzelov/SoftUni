abae1de25a9e3599033c11ac0143c1cb09975e15
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 18:39:58 +0200

    Add 03. Stack

---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/Stack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stack<T> : IEnumerable<T>
+{
+    private IList<T> _data;
+
+    public Stack()
+    {
+        this._data = new List<T>();
+    }
+
+    public void Push(params T[] elements)
+    {
+        foreach (var element in elements)
+        {
+            this._data.Add(element);
+        }
+    }
+
+    public void Pop()
+    {
+        int lastIndex = this._data.Count - 1;
+        if (lastIndex < 0)
+        {
+            throw new InvalidOperationException("No elements");
+        }
+
+        this._data.RemoveAt(lastIndex);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        int lastIndex = this._data.Count - 1;
+        for (int index = lastIndex; index >= 0; index--)
+        {
+            yield return this._data[index];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Stack<string> stack = new Stack<string>();
+
+        while (true)
+        {
+            string[] inputArgs = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string command = inputArgs[0];
+
+            string result = string.Empty;
+            switch (command)
+            {
+                case "Push":
+                    stack.Push(inputArgs.Skip(1).ToArray());
+                    break;
+
+                case "Pop":
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException ioe)
+                    {
+                        result = ioe.Message;
+                    }
+                    break;
+
+                case "END":
+                    result = string.Join($"{Environment.NewLine}", stack);
+                    result += Environment.NewLine + result;
+                    break;
+            }
+
+            if (result != string.Empty) Console.WriteLine(result);
+
+            if (command == "END") break;
+        }
+    }
+}
