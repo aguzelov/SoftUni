cf9dd56f0d3c3fb59630c22a8ff1d448a73d309f
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-24 00:51:30 +0200

    09. Linked List Traversal

---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/LinkedListTraversal/LinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    private class Node<T>
+    {
+        public Node<T> Next;
+        public T Value;
+    }
+
+    private Node<T> head;
+    private Node<T> current;
+    public int Count;
+
+    public LinkedList()
+    {
+        this.head = new Node<T>();
+        this.current = this.head;
+    }
+
+    public void AddAtLast(T data)
+    {
+        Node<T> newNode = new Node<T> { Value = data };
+        this.current.Next = newNode;
+        this.current = newNode;
+        this.Count++;
+    }
+
+    public void AddAtStart(T data)
+    {
+        Node<T> newNode = new Node<T>() { Value = data };
+        newNode.Next = this.head.Next;
+        this.head.Next = newNode;
+        this.Count++;
+    }
+
+    public void RemoveFromStart(T element)
+    {
+        if (this.Count > 0)
+        {
+            Node<T> curr = this.head;
+            Node<T> prev = curr;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+
+                Node<T> next = curr.Next;
+
+                if (curr.Value.Equals(element))
+                {
+                    prev.Next = next;
+                    this.Count--;
+                    return;
+                }
+
+                prev = curr;
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("No element exist in this linked list.");
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node<T> curr = this.head;
+        while (curr.Next != null)
+        {
+            curr = curr.Next;
+            yield return curr.Value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/LinkedListTraversal/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int numberOfCommands = int.Parse(Console.ReadLine());
+
+        LinkedList<string> linkedList = new LinkedList<string>();
+        while (numberOfCommands-- != 0)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            string command = tokens[0];
+
+            switch (command)
+            {
+                case "Add":
+                    string element = tokens[1];
+                    linkedList.AddAtLast(element);
+                    break;
+
+                case "Remove":
+                    element = tokens[1];
+                    linkedList.RemoveFromStart(element);
+                    break;
+            }
+        }
+
+        Console.WriteLine(linkedList.Count);
+        Console.WriteLine(string.Join(" ", linkedList));
+    }
+}
