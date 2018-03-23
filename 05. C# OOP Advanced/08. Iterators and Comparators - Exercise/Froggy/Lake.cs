5d30617c06ffc89f6e58310ce03176f001bd9154
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-03-23 19:16:42 +0200

    Add 04. Froggy

---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Lake : IEnumerable<int>
+{
+    private List<int> stones;
+
+    public Lake(params int[] strones)
+    {
+        this.stones = new List<int>(strones);
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int index = 1; index <= this.stones.Count; index++)
+        {
+            if (index % 2 != 0)
+                yield return this.stones[index - 1];
+        }
+
+        for (int index = this.stones.Count; index >= 1; index--)
+        {
+            if (index % 2 == 0)
+                yield return this.stones[index - 1];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
---
+++ 05. C# OOP Advanced/08. Iterators and Comparators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int[] stones = Console.ReadLine()
+            .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Lake lake = new Lake(stones);
+
+        Console.WriteLine(string.Join(", ", lake));
+    }
+}
