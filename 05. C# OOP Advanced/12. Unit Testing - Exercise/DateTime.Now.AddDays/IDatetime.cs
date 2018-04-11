d99afcea2ac2a55260468fb5f42f5d21064623f0
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-04-11 20:28:25 +0300

    Add DateTime.Now.AddDays

---
+++ 05. C# OOP Advanced/12. Unit Testing - Exercise/DateTime.Now.AddDays/IDatetime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IDatetime
+{
+    DateTime Now();
+
+    void AddDays(DateTime date, double daysToAdd);
+
+    TimeSpan SubstractDays(DateTime date, int daysToSybstract);
+}
