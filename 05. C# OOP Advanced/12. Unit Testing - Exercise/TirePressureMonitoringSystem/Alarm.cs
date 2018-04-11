7823366271d5529a9567bbf97a491019905a2c56
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-04-11 20:29:19 +0300

    Add Tire Pressure Monitoring System

---
+++ 05. C# OOP Advanced/12. Unit Testing - Exercise/TirePressureMonitoringSystem/Alarm.cs
@@ -0,0 +1,27 @@
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    public class Alarm
+    {
+        private const double LowPressureThreshold = 17;
+        private const double HighPressureThreshold = 21;
+
+        private readonly Sensor _sensor = new Sensor();
+
+        private bool _alarmOn = false;
+
+        public void Check()
+        {
+            double psiPressureValue = _sensor.PopNextPressurePsiValue();
+
+            if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+            {
+                _alarmOn = true;
+            }
+        }
+
+        public bool AlarmOn
+        {
+            get { return _alarmOn; }
+        }
+    }
+}
---
+++ 05. C# OOP Advanced/12. Unit Testing - Exercise/TirePressureMonitoringSystem/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TirePressureMonitoringSystem
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
