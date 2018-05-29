39736a107887c94c5a87e0fe17dbb1d9fcb50aad
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-05-29 14:58:25 +0300

    Rewrite exercise

--- 06. Database Basics - MS SQL/06. Basic CRUD/QueriesForSoftUniDatabase/21. Increase Salaries.sql
+++ 06. Database Basics - MS SQL/06. Basic CRUD/QueriesForSoftUniDatabase/21. Increase Salaries.sql
@@ -1,5 +1,5 @@
 UPDATE Employees
-SET Salary = Salary / 1.12
+SET Salary = Salary * 1.12
 WHERE DepartmentID IN (SELECT DepartmentID FROM Departments
 WHERE Name = 'Engineering' OR Name = 'Tool Design' OR Name = 'Marketing' OR Name = 'Information Services')
 
