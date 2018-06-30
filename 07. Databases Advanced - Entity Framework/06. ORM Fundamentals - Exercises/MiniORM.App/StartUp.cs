aca9816a8680542706de4c9e6b1e21119a81b97a
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-06-30 20:59:34 +0300

    Add 06. ORM Fundamentals - MiniORM - Exercises

---
+++ 07. Databases Advanced - Entity Framework/06. ORM Fundamentals - Exercises/MiniORM.App/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var connectionString = "Server=.;" +
+                "Database=MiniORM;" +
+                "Integrated Security = True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserte",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
