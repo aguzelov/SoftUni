4aa67c85a0ae55d163c6677c925911bd7e4a63ee
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 19:51:24 +0300

    Add 4. Employees with Salary Over 50 000

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using P02_DatabaseFirst.Data;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace EmployeesWithSalaryOver50000
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var employees = db.Employees
+                    .Where(e => e.Salary > 50000)
+                    .Select(e => new
+                    {
+                        e.FirstName
+                    })
+                    .OrderBy(e => e.FirstName)
+                    .ToList();
+
+                foreach (var employee in employees)
+                {
+                    FileWriter.WriteLine(employee.FirstName);
+                }
+            }
+        }
+    }
+}
