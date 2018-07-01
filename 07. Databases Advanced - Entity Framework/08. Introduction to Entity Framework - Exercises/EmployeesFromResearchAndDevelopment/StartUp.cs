beeb3b29bbae339381bb89363cd22214f5dd781d
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 19:50:53 +0300

    Add 5. Employees from Research and Development

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using P02_DatabaseFirst.Data;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace EmployeesFromResearchAndDevelopment
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var employees = db.Employees
+                    .Where(e => e.Department.Name == "Research and Development")
+                    .OrderBy(e => e.Salary)
+                    .ThenByDescending(e => e.FirstName)
+                    .Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        e.Department.Name,
+                        e.Salary
+                    })
+                    .ToList();
+
+                foreach (var employee in employees)
+                {
+                    FileWriter.WriteLine($"{employee.FirstName} {employee.LastName} from {employee.Name} - ${employee.Salary:F2}");
+                }
+            }
+        }
+    }
+}
