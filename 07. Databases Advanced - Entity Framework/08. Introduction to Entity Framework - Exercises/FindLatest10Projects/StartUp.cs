526d790e0370fdda034d1c0253382181a7aca4f0
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 22:23:00 +0300

    Add 11. Find Latest 10 Projects

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/FindLatest10Projects/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using P02_DatabaseFirst.Data;
+using System.Linq;
+using System;
+using System.Globalization;
+using ResultWriter;
+
+namespace FindLatest10Projects
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var projects = db.Projects
+                    .OrderByDescending(p => p.StartDate)
+                    .Take(10)
+                    .Select(p => new
+                    {
+                        p.Name,
+                        p.Description,
+                        StartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                    })
+                    .ToList();
+
+                foreach (var project in projects.OrderBy(p => p.Name))
+                {
+                    FileWriter.WriteLine(project.Name);
+                    FileWriter.WriteLine(project.Description);
+                    FileWriter.WriteLine(project.StartDate);
+                }
+            }
+        }
+    }
+}
