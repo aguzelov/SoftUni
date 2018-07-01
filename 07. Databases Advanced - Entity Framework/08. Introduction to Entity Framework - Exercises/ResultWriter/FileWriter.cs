ed8163840a5c5d0e3fa7c3d29af20e68a2c8099f
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 22:42:38 +0300

    Add ResultWriter

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/ResultWriter/FileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ResultWriter
+{
+    public static class FileWriter
+    {
+        private const string path = @"..\..\..\..\result.txt";
+
+        private static bool append = false;
+
+        public static void WriteLine(string text)
+        {
+            using (StreamWriter writer = new StreamWriter(path, append))
+            {
+                writer.WriteLine(text);
+            }
+
+            append = true;
+        }
+    }
+}
