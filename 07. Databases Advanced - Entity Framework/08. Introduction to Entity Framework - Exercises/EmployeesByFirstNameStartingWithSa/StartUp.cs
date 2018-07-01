5ef871aeede13bd5e74f1316d23616ab4319f453
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 22:47:54 +0300

    Add 13. Find Employees by First Name Starting With Sa

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/EmployeesByFirstNameStartingWithSa/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using P02_DatabaseFirst.Data;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace EmployeesByFirstNameStartingWithSa
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var employees = db.Employees
+                    .Where(e => e.FirstName.StartsWith("Sa"))
+                    .Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        e.JobTitle,
+                        e.Salary
+                    })
+                    .OrderBy(e => e.FirstName)
+                    .ThenBy(e => e.LastName)
+                    .ToList();
+
+                employees.ForEach(e => FileWriter.WriteLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:F2})"));
+            }
+        }
+    }
+}
