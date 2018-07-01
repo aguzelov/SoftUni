d75d6c95e16759937cdfbb1e35ec0f0186b8b7ac
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 21:10:59 +0300

    Add 8. Addresses by Town

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/AddressesByTown/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using P02_DatabaseFirst.Data;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace AddressesByTown
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var addresses = db.Addresses
+                    .OrderByDescending(a => a.Employees.Count)
+                    .ThenBy(a => a.Town.Name)
+                    .ThenBy(a => a.AddressText)
+                    .Take(10)
+                    .Select(a => new
+                    {
+                        AddressText = a.AddressText,
+                        TownName = a.Town.Name,
+                        EmployeeCount = a.Employees.Count
+                    })
+                    .ToList();
+
+                foreach (var address in addresses)
+                {
+                    FileWriter.WriteLine($"{address.AddressText}, {address.TownName} - {address.EmployeeCount} employees");
+                }
+            }
+        }
+    }
+}
