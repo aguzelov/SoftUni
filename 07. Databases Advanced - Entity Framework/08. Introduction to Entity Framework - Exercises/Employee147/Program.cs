b72571c718f5e63af80ba8c85fa766ddf313cafa
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 21:21:14 +0300

    Add 9. Employee 147

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/Employee147/Program.cs
@@ -0,0 +1,36 @@
+﻿using P02_DatabaseFirst.Data;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace Employee147
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var employee = db.Employees
+                    .Where(e => e.EmployeeId == 147)
+                    .Select(e => new
+                    {
+                        Name = $"{e.FirstName} {e.LastName}",
+                        Job = e.JobTitle,
+                        Project = e.EmployeesProjects.Select(ep => new
+                        {
+                            ep.Project.Name
+                        })
+                    })
+                    .SingleOrDefault();
+
+                FileWriter.WriteLine($"{employee.Name} - {employee.Job}");
+
+                foreach (var project in employee.Project.OrderBy(p => p.Name))
+                {
+                    FileWriter.WriteLine($"{project.Name}");
+                }
+            }
+        }
+    }
+}
