0ac8ccffa0461513abcaafc4c0f6e687f80115c9
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 22:08:29 +0300

    Add 10. Departments with More Than 5 Employees

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/DepartmentsWithMoreThan5Employees/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using P02_DatabaseFirst.Data;
+using System.Linq;
+using System;
+using ResultWriter;
+
+namespace DepartmentsWithMoreThan5Employees
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var departments = db.Departments
+                    .Where(d => d.Employees.Count > 5)
+                    .OrderBy(d => d.Employees.Count)
+                    .ThenBy(d => d.Name)
+                    .Select(d => new
+                    {
+                        DepartmentName = d.Name,
+                        ManagerName = $"{d.Manager.FirstName} {d.Manager.LastName}",
+                        Employees = d.Employees.Select(e => new
+                        {
+                            FirstName = e.FirstName,
+                            LastName = e.LastName,
+                            Job = e.JobTitle
+                        })
+                    })
+                    .ToList();
+
+                foreach (var department in departments)
+                {
+                    FileWriter.WriteLine($"{department.DepartmentName} - {department.ManagerName}");
+
+                    foreach (var employee in department.Employees.OrderBy(e => e.FirstName).ThenBy(e => e.LastName))
+                    {
+                        FileWriter.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.Job}");
+                    }
+                    FileWriter.WriteLine("----------");
+                }
+            }
+        }
+    }
+}
