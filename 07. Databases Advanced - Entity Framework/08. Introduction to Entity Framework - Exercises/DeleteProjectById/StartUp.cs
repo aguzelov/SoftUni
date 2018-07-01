d67ec705c84e22898700bfd96a62c7e7c065ab20
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 23:12:03 +0300

    Add 14. Delete Project by Id

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/DeleteProjectById/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using P02_DatabaseFirst.Data;
+using System.Linq;
+using System;
+using ResultWriter;
+
+namespace DeleteProjectById
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var projectToBeDeleted = db.Projects.FirstOrDefault(p => p.ProjectId == 2);
+
+                db.RemoveRange(db.EmployeesProjects
+                    .Where(ep => ep.ProjectId == 2));
+                db.Remove(projectToBeDeleted);
+                db.SaveChanges();
+
+                db.Projects
+                    .Select(p => p.Name)
+                    .Take(10)
+                    .ToList()
+                    .ForEach(p => FileWriter.WriteLine($"{p}"));
+            }
+        }
+    }
+}
