13a6486216e613b5ad13fbf351665e823e6c70f1
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-07-01 20:14:11 +0300

    Add 6. Adding a New Address and Updating Employee

---
+++ 07. Databases Advanced - Entity Framework/08. Introduction to Entity Framework - Exercises/AddingANewAddressAndUpdatingEmployee/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using P02_DatabaseFirst.Data;
+using P02_DatabaseFirst.Data.Models;
+using ResultWriter;
+using System;
+using System.Linq;
+
+namespace AddingANewAddressAndUpdatingEmployee
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var address = new Address();
+                address.AddressText = "Vitoshka 15";
+                address.TownId = 4;
+
+                Employee employee = db.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+                employee.Address = address;
+
+                db.SaveChanges();
+
+                var employees = db.Employees
+                    .OrderByDescending(e => e.AddressId)
+                    .Take(10)
+                    .Select(e => new
+                    {
+                        e.Address.AddressText
+                    })
+                    .ToList();
+
+                foreach (var a in employees)
+                {
+                    FileWriter.WriteLine(a.AddressText);
+                }
+            }
+        }
+    }
+}
