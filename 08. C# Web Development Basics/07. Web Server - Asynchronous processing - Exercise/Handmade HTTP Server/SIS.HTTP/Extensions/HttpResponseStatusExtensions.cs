0487bb44195e91e732e942dccffc5a5e64fec0ac
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-09-28 22:44:28 +0300

    Refactoring HttpResponseStatusCode extension method (removing switch)

--- 08. C# Web Development Basics/07. Web Server - Asynchronous processing - Exercise/Handmade HTTP Server/SIS.HTTP/Extensions/HttpResponseStatusExtensions.cs
+++ 08. C# Web Development Basics/07. Web Server - Asynchronous processing - Exercise/Handmade HTTP Server/SIS.HTTP/Extensions/HttpResponseStatusExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using SIS.HTTP.Enums;
+using System;
 
 namespace SIS.HTTP.Extensions
 {
@@ -6,39 +7,7 @@ namespace SIS.HTTP.Extensions
     {
         public static string GetResponseLine(this HttpResponseStatusCode code)
         {
-            switch (code)
-            {
-                case HttpResponseStatusCode.OK:
-                    return "200 OK";
-
-                case HttpResponseStatusCode.Created:
-                    return "201 Created";
-
-                case HttpResponseStatusCode.Found:
-                    return "302 Found";
-
-                case HttpResponseStatusCode.SeeOther:
-                    return "303 See Other";
-
-                case HttpResponseStatusCode.BadRequest:
-                    return "400 Bad Request";
-
-                case HttpResponseStatusCode.Unauthorized:
-                    return "401 Unauthorized";
-
-                case HttpResponseStatusCode.Forbidden:
-                    return "403 Forbidden";
-
-                case HttpResponseStatusCode.NotFound:
-                    return "404 Not Found";
-
-                case HttpResponseStatusCode.InternalServerError:
-                    return "500 Internal Server Error";
-
-                default:
-                    //TODO: Must change return message(need most appropriate)
-                    return "";
-            }
+            return $"{(int)code} {Enum.GetName(typeof(HttpResponseStatusCode), code)}";
         }
     }
 }
