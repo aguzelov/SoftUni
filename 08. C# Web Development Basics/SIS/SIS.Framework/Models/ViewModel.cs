0d5fad8233fb2ff3c7952d666171d54ee45096bb
Aleksandur Gyuzelov <aguzelov@outlook.com>
2018-10-15 01:39:31 +0300

    Introducing simple HTML View Engine

---
+++ 08. C# Web Development Basics/SIS/SIS.Framework/Models/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Framework.Models
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.Data = new Dictionary<string, object>();
+        }
+
+        public IDictionary<string, object> Data { get; }
+
+        public object this[string key]
+        {
+            get => this.Data[key];
+            set => this.Data[key] = value;
+        }
+    }
+}
