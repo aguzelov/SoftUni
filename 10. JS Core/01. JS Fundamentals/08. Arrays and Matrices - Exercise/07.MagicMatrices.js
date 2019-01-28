97c88173113ab19e73e52ff1c1639257ec323201
Aleksandur Gyuzelov <aguzelov@outlook.com>
2019-01-28 14:51:11 +0200

    Rewrite solution for 07. MagicMatrices

--- 10. JS Core/01. JS Fundamentals/08. Arrays and Matrices - Exercise/07.MagicMatrices.js
+++ 10. JS Core/01. JS Fundamentals/08. Arrays and Matrices - Exercise/07.MagicMatrices.js
@@ -1,42 +1,42 @@
 function solution(arr) {
-    let rowSum = 0;
-    let colSum = 0;
 
-    let isMagic = true;
+    console.log(isMagicMatrix(arr));
 
-    for (let i = 0; i < arr.length; i++) {
-        let currentRowSum = arr[i].reduce((a, b) => a + b, 0);
+    function isMagicMatrix(matrix) {
+        let sum = 0;
 
-        if (i === 0) {
-            rowSum = currentRowSum;
-        } else if (rowSum !== currentRowSum) {
-            isMagic = false;
-            break;
-        }
-
-        let currentColSum = 0;
-        for (let j = 0; j < arr[i].length; j++) {
-
-            if (i === 0) {
-                colSum += arr[i][j];
-                currentColSum += arr[i][j];
-            } else {
-                currentColSum += arr[i][j];
+        // For sums of Rows 
+        for (let i = 0; i < matrix.length; i++) {
+            let rowSum = 0;
+            for (let j = 0; j < matrix[i].length; j++) {
+                rowSum += +matrix[i][j];
             }
 
+            if (sum !== 0 && rowSum !== sum) {
+                return false;
+            }
+            sum = rowSum;
         }
 
-        if (colSum !== currentColSum) {
-            isMagic = false;
-            break;
+        // For sums of Columns 
+        for (let i = 0; i < matrix.length; i++) {
+            let colSum = 0;
+            for (let j = 0; j < matrix.length; j++) {
+                colSum += +matrix[j][i];
+            }
+
+            // check if every column sum is 
+            // equal to prime diagonal sum 
+            if (sum != colSum)
+                return false;
         }
+
+        return true;
     }
-
-    console.log(isMagic);
 }
 
 
-solution([[11, 32, 45],
-[21, 0, 1],
-[21, 1, 1]]
+solution([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]]
 );
