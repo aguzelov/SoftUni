ea5d1850f8b5bb6a6438e6c33f9f8aa5815e52cf
Aleksandur Gyuzelov <aguzelov@outlook.com>
2019-05-08 21:56:47 +0300

    implement login form and login model

--- 12. Angular Fundamentals/03. Forms/forms/src/app/authentication/login-form/login-form.component.ts
+++ 12. Angular Fundamentals/03. Forms/forms/src/app/authentication/login-form/login-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+	selector: 'app-login-form',
+	templateUrl: './login-form.component.html',
+	styleUrls: [ './login-form.component.css' ]
 })
 export class LoginFormComponent implements OnInit {
+	loginForm: FormGroup;
+	constructor() {}
 
-  constructor() { }
+	submitForm() {
+		console.log(this.loginForm);
+	}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.loginForm = new FormGroup({
+			username: new FormControl('', Validators.required),
+			password: new FormControl('', Validators.required)
+		});
+	}
 }
---
+++ 12. Angular Fundamentals/03. Forms/forms/src/app/authentication/models/login.model.ts
@@ -0,0 +1,4 @@
+export class LoginModel {
+	public username: string;
+	public password: string;
+}
