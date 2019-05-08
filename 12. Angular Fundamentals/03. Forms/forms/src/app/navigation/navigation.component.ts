3056a21efd0d2e405cd47b9188a4cb7ec3822dd8
Aleksandur Gyuzelov <aguzelov@outlook.com>
2019-05-08 23:45:00 +0300

    implement logout functionality

--- 12. Angular Fundamentals/03. Forms/forms/src/app/navigation/navigation.component.ts
+++ 12. Angular Fundamentals/03. Forms/forms/src/app/navigation/navigation.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/service/authentication.service';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+	selector: 'app-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: [ './navigation.component.css' ]
 })
 export class NavigationComponent implements OnInit {
+	constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
+	logout() {
+		console.log('logout');
 
-  ngOnInit() {
-  }
-
+		this.authService.logout();
+	}
+	ngOnInit() {
+		console.log('init navigation');
+	}
 }
